d7adb54e85335d6c9d7a16735d930eef0b1eaef3
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2016-12-05 21:06:49 -1000

    finished step 12

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -146,6 +146,14 @@ let domains = {
  *
  */
 
+let browsers = {
+  Chromium: 'google.com',
+  Safari: 'apple.com',
+  Opera: 'opera.com',
+  Firefox: 'mozilla.org',
+  Sleipnir: 'fenrir-inc.com',
+  Konqueror: 'konqueror.org'
+};
 
 /* Step 13
  *
