e100582bae4085926abf8606d5631828f7001d8f
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2016-12-06 17:35:37 -1000

    finished step 83

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1286,6 +1286,14 @@ Shape.prototype.getType = function() {
  *
  */
 
+Box.prototype.openBox = function() {
+  if ( this.isOpen === false ) {
+    this.isOpen = true;
+    return true;
+  } else {
+    return false;
+  }
+};
 
  /* Step 85
  *
