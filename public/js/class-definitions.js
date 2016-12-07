510450e2d71c3935bf8adb8e5542d82746bbc0ca
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2016-12-06 17:14:17 -1000

    finished step 78

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1184,7 +1184,7 @@ var chocolateChip = new Cookie('chocolate');
 var gingerbread = new Cookie('gingerbread');
 
 // Create 2 different meals
-var breakfast;
+var breakfast = new Meal(['cereal', 'milk']);
 var dinner;
 
 
