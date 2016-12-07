f629daf8e79d96e1776e2dd25a1b4fb2e3c42457
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2016-12-06 17:58:07 -1000

    finished step 90

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1391,6 +1391,16 @@ Cookie.prototype.swipedByCookieMonster = function(dayOfTheWeek) {
  *
  */
 
+Meal.prototype.containsJunkFood = function() {
+  let junkFood = ['chips', 'soda', 'ice cream', 'popcorn', 'candy'];
+  for ( let i = 0; i < junkFood.length; i ++ ) {
+    if ( this.foods.indexOf(junkFood[i]) > -1 ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
 
  /* Steps 91 to 100
  *
