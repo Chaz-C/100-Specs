054415583995328d39ca457c9537845a5821f233
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2016-12-06 17:21:30 -1000

    finished step 81

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1228,6 +1228,13 @@ Animal.prototype.isWarmBlooded = function() {
  *
  */
 
+Vehicle.prototype.drive = function(streetName) {
+  if ( typeof streetName === 'string' && streetName !== '' ) {
+    return `Driving on ${streetName}`;
+  } else {
+    return 'Driving forward';
+  }
+};
 
  /* Step 83
  *
